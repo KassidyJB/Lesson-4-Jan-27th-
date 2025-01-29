b91cad39ab32632a789ef0fa56019875a534ca58
KassidyJB <jeanbartkassidy51@gmail.com>
2025-01-29 23:53:10 +0000

    vowels define function pseudocode

---
+++ vowels(LESSON-5).py
@@ -0,0 +1,14 @@
+vowel = ['a', 'e', 'i', 'o', 'u']
+
+mess = input("Enter any phrase or word:  >")
+
+#DEF VOWEL (MESS)
+
+    #FOR CHAR IN MESS:
+        #VOWEL COUNT SET TO 0
+        #IF CHAR IN VOWELS:
+            #PRINT NUMBER OF VOWELS
+        #ELIF CHAR NOT IN VOWELS:
+            #PRINT "THERE ARE NO VOWELS"
+        #PRINT VOWEL COUNT
+pass
