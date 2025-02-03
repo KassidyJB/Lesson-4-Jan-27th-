e3bc085299bb2e3971276cf8f96a95238f38fe3b
KassidyJB <jeanbartkassidy51@gmail.com>
2025-02-03 22:31:54 +0000

    changes from lesson 4

---
+++ ArachnoPhonicsSTART(JAN29).py
@@ -0,0 +1,14 @@
+''''Psuedocode'''
+
+'''IMPORT RANDOM
+
+1. print welcoming message
+2. print number of spaces for random mystery
+word
+3. Get LEN() of mystery 
+3. Display Live/Tries count starting at 0,
+it'll decrease with each incorrect guess
+4. Create WHILE LOOP that keeps game going 
+as long as spider isn't fully built or life/try
+count isn't breached. 
+5. FOR LOOP nested within  '''
